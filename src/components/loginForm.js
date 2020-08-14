e27b16dd0acc0237590ea5bd5b4c1562520591a5
selinsnmz <selinsonmez111@gmail.com>
2020-08-14 15:51:56 +0300

    Firebase Connected with Project

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -10,14 +10,28 @@ export default class LoginForm extends Component{
 
     }
 
-    onButtonClicked(){
+    onButtonClicked = () => {
         const {email, password } = this.state;
+        console.log(email,password);
 
         firebase.auth().signInWithEmailAndPassword(email, password)
-        .catch(() => {
+        .then((result) => {
+            console.log("result", result)
+            if(result.user) {
+                alert("Giriş başarılı")
+            }
+        })
+        .catch((err) => {
+            console.log('error', err)
+            alert("Kullanıcı bulunamadı")
             firebase.auth().createUserWithEmailAndPassword(email, password)
-            .catch(() => {
-                 console.log('error');
+            .then((result) => {
+                console.log('result2', result);
+                alert("Kayıt başarılı")
+            })
+            .catch((error) => {
+                console.log('error', error);
+                alert("Kullanıcı oluşturulamadı")
             });
         });
     }
@@ -27,27 +41,27 @@ export default class LoginForm extends Component{
             <View style={{padding: 30}}>
                 <View>
                     <Input text='Email' inputPlaceHolder='Enter Email'
-                                        onChangeText={(text) => {
-                                            this.setState({
-                                                email:text
-                                            })
-                                        }}
-                                        value={this.state.email}
-                    />
+                    onChangeText={(text) => {
+                        this.setState({
+                            email:text
+                        })
+                    }}
+                    value={this.state.email}
+/>
                 </View>
                 <View>
                     <Input text='Password' inputPlaceHolder='Enter Password'
-                                        onChangeText={(text) => {
-                                            this.setState({
-                                                password:text
-                                            })
-                                        }}
-                                        secureTextEntry
-                                        value={this.state.password}
+                        onChangeText={(text) => {
+                            this.setState({
+                                password:text
+                            })
+                        }}
+                        secureTextEntry
+                        value={this.state.password}
                     />
                 </View>
                 <View  style={styles.buttonWrapper}>
-                    <Button onPress={this.onButtonClicked.bind(this)} color='#594484' title= 'LOGIN'/>
+                    <Button onPress={this.onButtonClicked} color='#594484' title= 'LOGIN'/>
                 </View>
             </View>
         )
